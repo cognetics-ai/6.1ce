e9837dbc887192ae168069076b6f3ffe739c8172
Cognetics AI <cognetics.ai@gmail.com>
2020-02-27 07:06:00 -0500

    bofa mvc

---
+++ tomcat-7.0.27/bin/setenv.sh
@@ -0,0 +1,2 @@
+JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
